23569ce17ea72ccbbee5c9b31664374280e657fa
Gerco Ballintijn <57731881+gcballintijn@users.noreply.github.com>
2024-09-21 11:56:34 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Coroutines
+Coroutine example code.
+
+Degenerate cases
+* cr_return_void -- does nothing, just falls through.
---
+++ cr_return_void.cxx
@@ -0,0 +1,41 @@
+//
+// cr_return_void.cxx -- Coroutines
+//
+// Degenerate coroutine, does nothing, just falls through.
+//
+
+#include <cstdlib>
+#include <coroutine>
+#include <iostream>
+
+struct Promise;
+
+struct Coroutine {
+    using promise_type = Promise;
+};
+
+struct Promise {
+    auto get_return_object()        { return Coroutine{}; }
+    auto initial_suspend()          { return std::suspend_never{}; }
+    auto final_suspend() noexcept   { return std::suspend_never{}; }
+    void return_void()              {}
+    void unhandled_exception()      {}
+};
+
+Coroutine
+coroutine()
+{
+    std::cout << "coroutine()" << std::endl;
+
+    co_return;
+}
+
+int
+main(int, char const **)
+{
+    std::cout << "Coroutine: return void" << std::endl;
+
+    coroutine();
+
+    return EXIT_SUCCESS;
+}
